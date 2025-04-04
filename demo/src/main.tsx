d2e8a4b6e80ac92efcae882a298946d4e1eb9bfc
David Hérault <dherault@gmail.com>
2025-04-04 22:26:04 +0300

    Edit demo

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -9,5 +9,6 @@ createRoot(document.getElementById('root')!).render(
     <Emoji name="red-heart" />
     <Emoji name="red-heart" width={16} />
     <Emoji name="kiss-man-person-no-skin-tone-medium-light-skin-tone" />
+    <Emoji name="index-pointing-at-the-viewer" />
   </EmojiProvider>
 )
